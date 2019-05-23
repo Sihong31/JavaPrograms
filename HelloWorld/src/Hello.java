dcb6e84e33701b528161af9a55b5cb349c904491
Sihong Huang <sihongspring2011@gmail.com>
2019-05-22 20:28:08 -0400

    intro to variable ints

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,17 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Sihongss!");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myFourthNumber = 1000;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myDifference = myFourthNumber - myTotal;
+
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
+        System.out.println(myTotal);
+        System.out.println(myDifference);
     }
 }
