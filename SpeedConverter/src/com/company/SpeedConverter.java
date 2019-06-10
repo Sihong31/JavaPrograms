f22dedc6a98d716bf0d8451c27ea661b82784b5b
Sihong Huang <sihongspring2011@gmail.com>
2019-06-09 20:59:51 -0400

    speed converter

---
+++ SpeedConverter/src/com/company/SpeedConverter.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if(kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609);
+    }
+}
